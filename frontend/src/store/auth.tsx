180440a7ed582077cf39cdc3a6d83affeb2db5a8
hamzah <shaikhhamzah93@gmail.com>
2025-07-24 23:21:42 +0530

    React vite setup | Zustand userauth store

---
+++ frontend/src/store/auth.tsx
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
+
+type AllUserData = {
+    user_id: string;
+    username?: string;
+}
+
+type AuthStore = {
+    allUserData: AllUserData | null,
+    loading: boolean,
+    user: () => {
+        user_id: string | null;
+        username: string | null;
+    },
+    setUser: (user: AllUserData) => void,
+    setLoading: (loading: boolean) => void,
+    isLoggedIn: () => boolean
+}
+
+const useAuthStore = create<AuthStore>((set, get) => ({
+    allUserData: null,
+    loading: false,
+
+    user: () => ({
+        user_id: get().allUserData?.user_id || null,
+        username: get().allUserData?.username || null,
+    }),
+
+    setUser: (user) => set({ allUserData: user }),
+
+    setLoading: (loading) => set({ loading }),
+
+    isLoggedIn: () => get().allUserData !== null,
+}))
+
+if (import.meta.env.DEV) {
+    mountStoreDevtool("Store", useAuthStore);
+}
+
+export { useAuthStore }
